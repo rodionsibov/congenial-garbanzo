dfc377d841a07cf500c31332c24bf81227f6078f
Rodion Sibov <rodionsibov@gmail.com>
2021-05-28 02:32:55 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app-wrapper">
+    <div class="app">
+      <Navigation />
+      <router-view />
+    </div>
   </div>
-  <router-view/>
 </template>
-
+<script>
+import Navigation from "@/components/Navigation.vue";
+export default {
+  components: { Navigation },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Quicksand", sans-serif;
 }
 
-#nav {
-  padding: 30px;
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.link {
+  cursor: pointer;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: black;
+}
+
+.link-light {
+  color: #fff;
 }
 </style>
