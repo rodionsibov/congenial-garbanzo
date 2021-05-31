e128a2d8c29d821b077c05f9811d369d62cdac49
Rodion Sibov <rodionsibov@gmail.com>
2021-05-31 19:46:25 +0300

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -55,4 +55,42 @@ export default {
 .arrow-light {
   color: white;
 }
+
+.blog-card-wrap {
+  position: relative;
+  padding: 80px 16px;
+  background-color: #f1f1f1;
+}
+
+@media (min-width: 500) {
+  .blog-card-wrap {
+    padding: 100px 16px;
+  }
+}
+
+.blog-card-wrap .blog-cards {
+  display: grid;
+  gap: 32px;
+  grid-template-columns: 1fr;
+}
+
+@media (min-width: 500px) {
+  .blog-card-wrap .blog-cards {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (min-width: 900px) {
+  .blog-card-wrap .blog-cards {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media (min-width: 1200px) {
+  .blog-card-wrap .blog-cards {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,15 +6,24 @@
       :key="index"
       :post="post"
     />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCard v-for="(post, index) in sampleBlogCards" :key="index" :post="post" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPost from "@/components/BlogPost.vue";
+import BlogCard from "@/components/BlogCard.vue";
 
 export default {
   name: "Home",
-  components: { BlogPost },
+  components: { BlogPost, BlogCard },
   data() {
     return {
       welcomeScreen: {
@@ -22,21 +31,51 @@ export default {
         blogPost:
           "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post",
         welcomeScreen: true,
-        photo: 'https://picsum.photos/400?random=1',
+        photo: "https://picsum.photos/400?random=1",
       },
       sampleBlogPost: [
         {
           title: "This is a Filter Title!",
           blogHTML: "This is a filter blog post title!",
-          blogCoverPhoto: 'https://picsum.photos/400?random=2',
+          blogCoverPhoto: "https://picsum.photos/400?random=2",
         },
         {
           title: "This is a Filter Title!",
           blogHTML: "This is a filter blog post title!",
-          blogCoverPhoto: 'https://picsum.photos/400?random=3',
+          blogCoverPhoto: "https://picsum.photos/400?random=3",
+        },
+      ],
+      sampleBlogCards: [
+        {
+          blogTitles: "Blog Card #1",
+          blogCoverPhoto: "https://picsum.photos/400?random=4",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitles: "Blog Card #2",
+          blogCoverPhoto: "https://picsum.photos/400?random=5",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitles: "Blog Card #3",
+          blogCoverPhoto: "https://picsum.photos/400?random=6",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitles: "Blog Card #4",
+          blogCoverPhoto: "https://picsum.photos/400?random=7",
+          blogDate: "May 1, 2021",
         },
       ],
     };
   },
 };
 </script>
+
+<style scoped>
+.blog-card-wrap h3 {
+  font-weight: 300;
+  font-size: 28px;
+  margin-bottom: 32px;
+}
+</style>
